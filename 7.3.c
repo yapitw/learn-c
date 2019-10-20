bab2b4750d1c28c6405bdf023ff9adf981b6b69a
Pattison Ho <yapi.tw@gmail.com>
2019-10-21 00:59:27 +0800

    add 7.3

---
+++ 7.3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  int num1, num2;
+  char op;
+  float answer;
+
+  printf("%d", '+');
+  scanf("%d%c%d", &num1, &op, &num2);
+
+  if (op == '+') {
+    answer = num1 + num2;
+  } else if (op == '-') {
+    answer = num1 - num2;
+  } else if (op == '*') {
+    answer = num1 * num2;
+  } else if (op == '/') {
+    answer = (float)num1 / num2;
+  }
+  printf("ANS: %f\n", answer);
+  return 0;
+}
