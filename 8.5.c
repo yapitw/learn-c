685cfc8c99e4e7d4d6e9e1076aed263d949a4912
yapi <yapi.tw@gmail.com>
2019-10-25 00:33:27 +0800

    learning to 8.6

--- 8.5.c
+++ 8.5.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main() {
+  int N;
+  printf("Please enter the number: ");
+  scanf("%d", &N);
   int number = 0;
-  while (number < 7) {
+  while (number < N) {
     number++;
     printf("%d\n", number);
   }
   return 0;
-}
+} 
