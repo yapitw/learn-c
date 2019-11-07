be5fdf0861d95c882c60a2006a694cb64c743f56
yapi <yapi.tw@gmail.com>
2019-11-08 01:06:33 +0800

    update reader writer modules

--- image-processing-modular/main.c
+++ image-processing-modular/main.c
@@ -5,43 +5,30 @@
 #define BMP_COLOR_TABLE_SIZE 1024
 #define CUSTOM_IMG_SIZE 1024 * 1024
 
+void imageReader(const char* imgName, int* _height, int* _width, int* _bitDepth,
+                 unsigned char* _header, unsigned char* _colorTable,
+                 unsigned char* _buf);
+
+void imageWriter(const char* imgName, unsigned char* header,
+                 unsigned char* colorTable, unsigned char* buf, int bitDepth);
+
 int main() {
-  FILE* streamIn = fopen("images/cameraman.bmp", "rb");
-  FILE* fo       = fopen("images/cameraman_copy.bmp", "wb");
 
-  if (streamIn == (FILE*)0) {
-    printf("Unable to open file\n");
-  }
-  unsigned char header[54];
-  unsigned char colorTable[1024];
+  int imgWidth, imgHeight, imgBitDepth;
+  unsigned char imgHeader[BMP_HEADER_SIZE];
+  unsigned char imgColorTable[BMP_COLOR_TABLE_SIZE];
+  unsigned char imgBuffer[CUSTOM_IMG_SIZE];
 
-  // for (int i = 0; i < 54; i++) {
-  //   header[i] = getc(streamIn);
-  // }
-  fread(header, sizeof(unsigned char), 54, streamIn);
+  const char imgName[]    = "images/cameraman.bmp";
+  const char newImgName[] = "images/cameraman_copy.bmp";
 
-  int width    = *(int*)&header[18];
-  int height   = *(int*)&header[22];
-  int bitDepth = *(int*)&header[28];
+  imageReader(imgName, &imgHeight, &imgWidth, &imgBitDepth, //
+              &imgHeader[0], &imgColorTable[0], &imgBuffer[0]);
 
-  if (bitDepth <= 8) {
-    fread(colorTable, sizeof(unsigned char), 1024, streamIn);
-  }
+  imageWriter(newImgName, imgHeader, imgColorTable, imgBuffer, imgBitDepth);
 
-  fwrite(header, sizeof(unsigned char), 54, fo);
-  unsigned char buf[height * width];
-  fread(buf, sizeof(unsigned char), (height * width), streamIn);
+  printf("SUCCESS !\n");
 
-  if (bitDepth <= 8) {
-    fwrite(colorTable, sizeof(unsigned char), 1024, fo);
-  }
-  fwrite(buf, sizeof(unsigned char), (height * width), fo);
-  fclose(fo);
-  fclose(streamIn);
-
-  printf("Success!\n");
-  printf("Width: %d\n", width);
-  printf("Height: %d\n", height);
   return 0;
 }
 
@@ -74,4 +61,20 @@ void imageReader(               //
 
   fread(_buf, sizeof(unsigned char), CUSTOM_IMG_SIZE, streamIn);
   fclose(streamIn);
+}
+
+void imageWriter(              //
+    const char* imgName,       //
+    unsigned char* header,     //
+    unsigned char* colorTable, //
+    unsigned char* buf,        //
+    int bitDepth               //
+) {
+  FILE* fo = fopen(imgName, "wb");
+  fwrite(header, sizeof(unsigned char), 54, fo);
+  if (bitDepth <= 8) {
+    fwrite(colorTable, sizeof(unsigned char), 1024, fo);
+  }
+  fwrite(buf, sizeof(unsigned char), CUSTOM_IMG_SIZE, fo);
+  fclose(fo);
 }
